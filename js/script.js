6e5c70b1ac670958883e667a5376a09271d88e7d
Helen Moskovskaya <helenmoskovskaya2gmail.com>
2017-12-07 23:25:31 +0300

    final-3

--- js/script.js
+++ js/script.js
@@ -3,26 +3,31 @@ var link = document.querySelector(".button-write-us");
 var popup = document.querySelector(".modal-write-us");
 var close = popup.querySelector(".modal-close");
 
-var form = popup.querySelector(".modal-form");
+var form = popup.querySelector("form");
 var name = popup.querySelector("[name=user-name]");
-var mail = popup.querySelector("[name=e-mail");
-var text = popup.querySelector("[name=text-mail") 
+var mail = popup.querySelector("[name=e-mail]");
+var text = popup.querySelector("[name=text-field]") 
 
-var storage = localStorage.getItem("name");
-var storage = localStorage.getItem("e-mail");
+var storage = localStorage.getItem("user-name");
+var storage = localStorage.getItem("mail");
 
 
 link.addEventListener("click", function(event) {
   event.preventDefault();
-  popup.classList.add ("modal-show");
+  popup.classList.add("modal-show");
 
   if (storage) {
     name.value = storage;
     mail.focus();
-  } else {
-    name.focus();
+
+  if (storage) {
+    mail.value = storage;
+    text.focus();
+  } else { 
+    mail.focus();
   }
-});
+  } 
+})
 
 close.addEventListener ("click", function (event) {
   event.preventDefault();
@@ -30,17 +35,20 @@ close.addEventListener ("click", function (event) {
   popup.classList.remove ("modal-error");
   });
 
-form.addEventListener("submit", function(event) {
+ form.addEventListener("submit", function(event) {
   if (!name.value || !mail.value) {
   event.preventDefault();
   popup.classList.remove("modal-error");
+  popup.offsetWidth = popup.offsetWidth;
+  popup.classList.add("modal-error");
   } else {
-    localStorage.selItem("name", name.value)
+    localStorage.selItem("user-name", name.value);
+    localStorage.selItem("mail", mail.value);
   }
 });
 
 window.addEventListener("keydown", function (event) {
-  if (evt.keyCode === 27) {
+  if (event.keyCode === 27) {
     if (popup.classList.contains ("modal-show")){
        popup.classList.remove("modal-show");
        popup.classList.remove("modal-error");
@@ -48,20 +56,21 @@ window.addEventListener("keydown", function (event) {
   }
 })
 
-  var mapOpen = document.querySelector(".map-conteiner");
+
+  var mapOpen = document.querySelector(".info-map");
 
   var mapPopup = document.querySelector(".modal-map");
-  var mapClose = mapPopup.querySelector(".close-map");
+  var mapClose = mapPopup.querySelector(".modal-map-close");
 
 
   mapOpen.addEventListener("click", function (evt) {
     evt.preventDefault();
-    mapPopup.classList.add("modal-show");
+    mapPopup.classList.add("modal-map-show");
   });
 
   mapClose.addEventListener("click", function (evt) {
     evt.preventDefault();
-    mapPopup.classList.remove("modal-show");
+    mapPopup.classList.remove("modal-map-show");
   });
 
   window.addEventListener("keydown", function (evt) {
